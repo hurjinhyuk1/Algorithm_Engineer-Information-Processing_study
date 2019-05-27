473136c4dc4faae734ab4913091e4eaf86178e02
hurjinhyuk1 <jinhhur1@naver.com>
2019-05-27 13:10:44 +0900

    0527

---
+++ CodeUp/1101~1200/1170.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main (void)
+{
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+    printf("%d%d%02d",a,b,c);
+	return 0;
+}
---
+++ CodeUp/1101~1200/1171.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main (void)
+{
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+    printf("%d%02d%03d",a,b,c);
+	return 0;
+}
